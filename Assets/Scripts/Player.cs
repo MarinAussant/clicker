d15c99e0a63987971ec702e5a7f4ea88183f0c84
Marin Aussant <118370229+MarinAussant@users.noreply.github.com>
2024-01-20 15:08:53 +0100

    Final 1.0

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,14 +15,21 @@ public class Player : MonoBehaviour
     private GameObject selectedThrowableObject;
 
 
-
     public float stellar;
 
-    public float autoClickSpeed = 1f;
 
-    [SerializeField] private float clickCooldown = 3;
+    [SerializeField] private TextMeshProUGUI uiTextRocket;
+    private bool rocketUnlocked = false;
 
-    public bool autoClick = false;
+    [SerializeField] private TextMeshProUGUI uiTextLibertyShip;
+    private bool LibertyShipUnlocked = false;
+
+
+    [SerializeField] private TextMeshProUGUI uiTextSatelite;
+    private float newSatelitePrice = 200f;
+
+    [SerializeField] private TextMeshProUGUI uiTextDeathStar;
+    private float newDeathStarPrice = 5000f;
     
 
     // Start is called before the first frame update
@@ -47,7 +54,6 @@ public class Player : MonoBehaviour
                         GameObject throwableObject = Instantiate(selectedThrowableObject, Camera.main.transform.position, Camera.main.transform.parent.transform.rotation);
                         throwableObject.transform.position += (Vector3.zero - throwableObject.transform.position) / 20;
                         throwableObject.GetComponent<Rigidbody>().AddForce((ray.GetPoint(15) - throwableObject.transform.position) * selectedThrowableObject.GetComponent<ThrowableObject>().speed);
-                        // GERER Rotation de l'asteroid ICI
                     }
                 }
             }
@@ -66,15 +72,89 @@ public class Player : MonoBehaviour
 
     public void SelectedThrowableObject(int index)
     {
-        selectedThrowableObject = throwableObject[index];
+
+        switch (index) {
+            case 0:
+                selectedThrowableObject = throwableObject[index];
+                break;
+
+            case 1:
+                if (rocketUnlocked)
+                {
+                    selectedThrowableObject = throwableObject[index];
+                }
+                else
+                {
+                    if (stellar >= 500)
+                    {
+                        getMoney(-500);
+                        uiTextRocket.color = new Color(136f/255f, 255f/255f, 136f/255f, 255f/255f);
+                        uiTextRocket.text = "Unlocked";
+                        rocketUnlocked = true;
+                        selectedThrowableObject = throwableObject[index];
+                    }
+                }
+                break;
+
+            case 2:
+                if (LibertyShipUnlocked)
+                {
+                    selectedThrowableObject = throwableObject[index];
+                }
+                else
+                {
+                    if (stellar >= 10000)
+                    {
+                        getMoney(-10000);
+                        uiTextLibertyShip.color = new Color(136f / 255f, 255f / 255f, 136f / 255f, 255f / 255f);
+                        uiTextLibertyShip.text = "Unlocked";
+                        LibertyShipUnlocked = true;
+                        selectedThrowableObject = throwableObject[index];
+                    }
+                }
+                break;
+        }
+        
     }
 
     public void PlaceAutoShootHelper(int index)
     {
-        GameObject autoShootHelper = Instantiate(autoShootHelperList[index]);
-        autoShootHelper.transform.SetParent(GameObject.Find("ShipRotator").transform);
-        autoShootHelper.transform.position = new Vector3(autoShootHelper.GetComponent<autoShoot>().distance, 0, 0);
-        autoShootHelper.transform.RotateAround(autoShootRotator.transform.position, new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)), Random.Range(-180f, 180f));
+
+        switch (index)
+        {
+            case 0:
+                if (stellar >= newSatelitePrice)
+                {
+                    //Prix
+                    getMoney(-newSatelitePrice);
+                    newSatelitePrice *= 2;
+                    uiTextSatelite.text = newSatelitePrice + "Ṩ";
+
+                    //Placement object
+                    GameObject autoShootHelper = Instantiate(autoShootHelperList[index]);
+                    autoShootHelper.transform.SetParent(GameObject.Find("ShipRotator").transform);
+                    autoShootHelper.transform.position = new Vector3(autoShootHelper.GetComponent<autoShoot>().distance, 0, 0);
+                    autoShootHelper.transform.RotateAround(autoShootRotator.transform.position, new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)), Random.Range(-180f, 180f));
+                }
+                break;
+
+            case 1:
+                if (stellar >= newDeathStarPrice)
+                {
+                    //Prix
+                    getMoney(-newDeathStarPrice);
+                    newDeathStarPrice *= 2;
+                    uiTextDeathStar.text = newDeathStarPrice + "Ṩ";
+
+                    //Placement object
+                    GameObject autoShootHelper = Instantiate(autoShootHelperList[index]);
+                    autoShootHelper.transform.SetParent(GameObject.Find("ShipRotator").transform);
+                    autoShootHelper.transform.position = new Vector3(autoShootHelper.GetComponent<autoShoot>().distance, 0, 0);
+                    autoShootHelper.transform.RotateAround(autoShootRotator.transform.position, new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)), Random.Range(-180f, 180f));
+                }
+                break;
+        }
+        
     }
 
 }
